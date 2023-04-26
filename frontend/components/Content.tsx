2d726360bc7003128e52d66aab5a16d11816d2cb
Ninym2545 <Ninym2545@gmail.com>
2023-04-26 22:45:25 +0700

    Add Contentbody

--- frontend/components/Content.tsx
+++ frontend/components/Content.tsx
@@ -1,14 +1,67 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import {
+  CurrencyDollarIcon
+} from "@heroicons/react/24/outline";
 export default function Content() {
   return (
     <div>
       <div className="contentheader">
         <h3>Dashboard Overview</h3>
       </div>
-      <div className="">
-
+      <div className="contentbody">
+        <div className="card">
+          <div className="card-header">
+            <label>Total Income</label>
+            <div className="box-icons">
+              <div className="icons">
+                <CurrencyDollarIcon />
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <label>579,000</label>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <label>Total Income</label>
+            <div className="box-icons">
+              <div className="icons">
+                <CurrencyDollarIcon />
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <label>579,000</label>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <label>Total Income</label>
+            <div className="box-icons">
+              <div className="icons">
+                <CurrencyDollarIcon />
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <label>579,000</label>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <label>Total Income</label>
+            <div className="box-icons">
+              <div className="icons">
+                <CurrencyDollarIcon />
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <label>579,000</label>
+          </div>
+        </div>
       </div>
     </div>
   );
